1e3c33b989624c0abac319598279806aee9703f4
KarineA <84214373+KarineA213@users.noreply.github.com>
2025-01-17 23:45:21 -0300

    Add files via upload

---
+++ TesteSpring1Application.java
@@ -0,0 +1,15 @@
+package com.example.TesteSpring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteSpring1Application.class, args);
+
+		System.out.println("hello world");
+	}
+
+}
